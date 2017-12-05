c3fde4019d0e4a41867e3b34873cf567ee45ca66
TAKIO-PC\takio <taka21centry@gmail.com>
2017-12-06 06:59:55 +0900

    色々な書き方があることを理解

--- Chapter2-1/Program.cs
+++ Chapter2-1/Program.cs
@@ -10,6 +10,24 @@ namespace Chapter2_1
     {
         static void Main(string[] args)
         {
+            /* var first = new Song("ss","as",1 );
+             var songs = new Song[10];
+
+             for (int i = 0; i < 10; ++i)
+                 songs[i] = new Song("ss","aa",1);
+             */
+
+            var songs = new Song[]
+            {
+               new Song("君の名は","米津健司",1),
+               new Song("あなたに","米倉真紀子",2)
+            };
+
+            foreach(var s in songs)
+            {
+                Console.WriteLine($"{s.Title}{s.ArtistName}{s.Length}");
+            }
+
         }
     }
 }
---
+++ Chapter2-1/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2_1
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title, string artistname,int length)
+        {
+            this.Title = title;
+            this.ArtistName = artistname;
+            this.Length = length;
+        } 
+    }
+}
